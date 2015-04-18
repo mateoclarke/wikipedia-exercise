2942f43372ff28fcc00ca0adc1c4a5a753df5650
mateoclarke <code@mateoclarke.com>
2015-04-17 20:35:07 -0500

    Update 2015-04-18T01:34:55.539Z

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $('.timeline-point').hover(function(){
 	var fact = $(this).attr('data-tooltip');
 	var circle = $(this).find('.outer-circle-dot');
 	var offset = circle.offset();
+	
 	$('<div class="tooltip"><p>'+fact+'</p><div class="tooltip-arrow"></div></div>').prependTo($(this));
 	var ttWidth = ($('.tooltip').width()/2) - 12;
 	var ttHeight = $('.tooltip').height() + 15;
