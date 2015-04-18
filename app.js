1598fc55cba3b2cf9ff6a50336e798babc23171a
mateoclarke <code@mateoclarke.com>
2015-04-17 20:30:38 -0500

    Add tooltip

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('test');
+$('.timeline-point').hover(function(){
+	var fact = $(this).attr('data-tooltip');
+	var circle = $(this).find('.outer-circle-dot');
+	var offset = circle.offset();
+	$('<div class="tooltip"><p>'+fact+'</p><div class="tooltip-arrow"></div></div>').prependTo($(this));
+	var ttWidth = ($('.tooltip').width()/2) - 12;
+	var ttHeight = $('.tooltip').height() + 15;
+	$('.tooltip').css({top: offset.top - ttHeight, left: offset.left - ttWidth});
+}, function(){
+	$('.tooltip').remove();
+})
+
