d99ef22f741a98edf0b36e583f95d1bea013f3a9
mateoclarke <code@mateoclarke.com>
2015-04-22 19:01:26 -0500

    Refactor app.js to better encapsulate functions and define variables

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-$('.timeline-point').hover(function(){
+;(function(){
 
-	var $this = $(this);
-	var fact = $this.attr('data-tooltip');
-	var circleOffset = $this.find('.outer-circle-dot').offset();
-	
-	$('<div class="tooltip"><p>'+fact+'</p><div class="tooltip-arrow"></div></div>').appendTo($('body'));
+	"use strict";
 
-	var ttWidthCenter = ($('.tooltip').width())/2 - 12;
-	var ttHeightAdjust = $('.tooltip').height() + 15;
+	var $tooltipEvent = $('.timeline-point');
 
-	$('.tooltip').css({
-		top: circleOffset.top - ttHeightAdjust, 
-		left: circleOffset.left - ttWidthCenter
-	});
+	$tooltipEvent.hover( showTooltip, hideTooltip );
 
-}, function(){
-	$('.tooltip').remove();
-})
+	function showTooltip(){
+		var $this 			= $(this),
+			$body			= $('body'),
+			$fact 			= $this.attr('data-tooltip'),
+			$circle 		= $this.find('.outer-circle-dot'),
+			$circleOffset 	= $circle.offset(),
+			circleTop		= $circleOffset.top,
+			circleLeft		= $circleOffset.left,
+			$tooltipHTML		= $('<div class="tooltip"><p>'+ $fact +'</p><div class="tooltip-arrow"></div></div>'),
+			ttWidthCenter,
+			ttHeightAdjust;
+		
+		hideTooltip();
+		$tooltipHTML.appendTo($body);
 
+		ttWidthCenter = ($('.tooltip').width())/2 - 12;
+		ttHeightAdjust = $('.tooltip').height() + 15;
+
+		$('.tooltip').css({
+			top: circleTop - ttHeightAdjust, 
+			left: circleLeft - ttWidthCenter
+		});
+
+	}
+
+	function hideTooltip(){
+		$('.tooltip').remove();
+	}
+
+}());
